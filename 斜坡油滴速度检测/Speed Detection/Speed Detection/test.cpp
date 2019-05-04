922a8253fed309656fd08ca9a659cf59774cbc6e
ClivenZ <835105614@qq.com>
2019-05-04 20:10:32 +0800

    斜坡油滴速度检测

---
+++ 斜坡油滴速度检测/Speed Detection/Speed Detection/test.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+void ReadData() {
+	Mat pic;
+	cv::imread("pic.jpg");
+
+
+}
+
+
+int mian() {
+
+
+	return 0;
+}
